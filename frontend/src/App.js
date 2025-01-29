a336941e1e8dc38f86ce40c9288cd2f7a98fa9ca
prabhat-4165 <prabhu27082001@gmail.com>
2025-01-29 14:51:23 +0530

    added app.js in frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/dashboard";
+import PrivateRoute from "./components/PrivateRoutes";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
